f72ada185b9e3c05175a77ed0e36ccd6d9b973e5
geomancer <>
2023-03-30 16:53:04 -0600

    I got 99 problems but a borrow checker ain't one

--- src/effects/inventory.rs
+++ src/effects/inventory.rs
@@ -1,12 +1,14 @@
-use shipyard::{ViewMut, Get, UniqueView, UniqueViewMut, View};
+use shipyard::{ViewMut, Get, UniqueView, UniqueViewMut, View, Remove, AddComponent};
 
 use super::*;
-use crate::{components::{Position, Inventory, InBackpack, WantsToPickupItem, Name, Equipped}, utils::{PlayerID}, gamelog::GameLog};
+use crate::{components::{Position, Inventory, InBackpack, Name, Equipped, WantsToPickupItem}, utils::{PlayerID}, gamelog::GameLog};
 
 pub fn pick_up(gs: &mut State, effect: &EffectSpawner, target: EntityId) {
-    let vpos = gs.world.borrow::<View<Position>>().unwrap();
+    let mut vpos = gs.world.borrow::<ViewMut<Position>>().unwrap();
     let vname = gs.world.borrow::<View<Name>>().unwrap();
     let vinv = gs.world.borrow::<View<Inventory>>().unwrap();
+    let mut vwantspickup = gs.world.borrow::<ViewMut<WantsToPickupItem>>().unwrap();
+    let mut vpacks = gs.world.borrow::<ViewMut<InBackpack>>().unwrap();
 
     if let Some(id) = effect.creator {
         let mut log = gs.world.borrow::<UniqueViewMut<GameLog>>().unwrap();
@@ -27,7 +29,7 @@ pub fn pick_up(gs: &mut State, effect: &EffectSpawner, target: EntityId) {
             return;
         }
     
-        if let Ok(name) = vname.get(id) {
+        if let Ok(_) = vname.get(id) {
             if let Ok(inv) = vinv.get(id) {
                 inv.items.push(target);
 
@@ -50,38 +52,38 @@ pub fn pick_up(gs: &mut State, effect: &EffectSpawner, target: EntityId) {
             }
         }
     
-        let _res = gs.world.remove::<Position>(target);
-        let _r = gs.world.add_component(target, InBackpack {owner: id});
+        let _res = vpos.remove(target);
+        let _r = vpacks.add_component_unchecked(target, InBackpack {owner: id});
     
         if id == player_id {
             let name = vname.get(target).unwrap();
             log.messages.push(format!("You pick up the {}", name.name));
         }
     
-        let _re = gs.world.remove::<WantsToPickupItem>(id);    
+        let _re = vwantspickup.remove(id);
     }
 }
 
 pub fn drop_item(gs: &mut State, effect: &EffectSpawner, target: EntityId) {
-    let vpos = gs.world.borrow::<View<Position>>().unwrap();
+    let mut vpos = gs.world.borrow::<ViewMut<Position>>().unwrap();
     let vinv = gs.world.borrow::<View<Inventory>>().unwrap();
+    let mut vpack = gs.world.borrow::<ViewMut<InBackpack>>().unwrap();
+    let mut vequipped = gs.world.borrow::<ViewMut<Equipped>>().unwrap();
 
     if let Some(id) = effect.creator {
-        let world = &mut gs.world;
-
         let pos = if let Ok(p) = vpos.get(id) {
             p.any_point()
         }else{
             unreachable!()
         };
-        if let Ok(mut inv) = vinv.get(id) {
+        if let Ok(inv) = vinv.get(id) {
             if let Some(pos) = inv.items.iter().position(|x| *x == target) {
                 inv.items.remove(pos);
             }
         }
         
-        let _in_bp = world.remove::<InBackpack>(id);
-        let _equipped = world.remove::<Equipped>(id);
-        world.add_component(target, Position { ps:vec![pos]});
+        vpack.remove(id);
+        vequipped.remove(id);
+        vpos.add_component_unchecked(target, Position { ps:vec![pos]});
     }
 }
